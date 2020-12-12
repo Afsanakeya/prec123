450a453a20e8a1a85ffde26338a965c33c07a7bf
afsanakeya <afsanakeya@localhost>
2020-12-11 21:19:22 -0500

    commit

--- src/base/Config.java
+++ src/base/Config.java
@@ -13,35 +13,46 @@ import org.testng.annotations.Parameters;
 public class Config extends WDFunctions {
 
 	@BeforeSuite
-	public void startTestSuite(){
+	public void startTestSuite() {
 		getCurrentTime();
-		}
-		
+	}
+
 	@Parameters("browser")
 	@BeforeMethod
-	public void beforeEachTestMethod(String browser){
-		if (browser.equalsIgnoreCase("ch")){
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver");
-			driver = new ChromeDriver(); 
-			System.out.println (" chrome driver open ++++++++++++++++ ");
-		} else if (browser.equalsIgnoreCase("ff")){
-			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver");
+	public void beforeEachTestMethod(String browser) {
+		if (browser.equalsIgnoreCase("ch")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+			driver = new ChromeDriver();
+			System.out.println(" chrome driver open ++++++++++++++++ ");
+		} 
+		else if (browser.equalsIgnoreCase("ff")) {
+			System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver ");
 			driver = new FirefoxDriver(); 
 			System.out.println (" Firefox driver open ++++++++++++++++ ");
-		}}
-		
-		
-		@AfterMethod
-		public void afterEachTestMethod(){
-			// close browser
-			driver.close();
-			System.out.println (" Test is ended and browser is closeded +++++++ ");
 		}
-		
-		@AfterSuite
-		public void tearDown(){
-			driver.quit();
-			System.out.println (" Terminated WD +++++++ ");
+		else if(browser.equalsIgnoreCase("opera")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver");
+			driver = new FirefoxDriver();
+			System.out.println(" Firefox driver open ++++++++++++++++ ");
 		}
-
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+		// open browser	
+		driver.get("http://www.booking.com");
 	}
+
+	@AfterMethod
+	public void afterEachTestMethod() {
+		// close browser
+		driver.close();
+		System.out.println(" Test is ended and browser is closeded +++++++ ");
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+		System.out.println(" Terminated WD +++++++ ");
+	}
+
+}
